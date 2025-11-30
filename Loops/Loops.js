14e53fea63c2e71746f42785b460eb60737cdb32
Anamul Haque <anamulhaque4631@gmail.com>
2025-11-30 22:51:23 +0600

    LOops

---
+++ Loops/Loops.js
@@ -0,0 +1,79 @@
+//  for loop
+// for (expr1; expr2; expr3) {
+//     // code block to be executed
+// };
+
+let text;
+// for (let i = 0; i < 5; i++) {
+//     text += "The number" + i;
+// };
+
+// loop scope
+// let i = 5;
+// for (i = 0; i < 10; i++) {
+
+//     // some code
+
+// }; // here i = 10
+
+// while loop
+
+// while (condition) {
+//     // code block to be executed
+// }
+
+let number;
+while (number < 20) {
+    text += "the number" + i;
+    number++;
+
+};
+
+
+//  do while loop
+
+// do {
+//    // code block to be executed
+// } while (condition); 
+
+
+// do {
+//     text += "The number" + i;
+// } while (i < 10);
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let len = cars.length;
+
+let texts = "";
+for (let i = 0; i < len; i++) {
+    texts += cars[i];
+}
+//console.log(texts);
+
+//
+//JS break
+// for (let i = 0; i < 10; i++) {
+//     if (i === 4) {
+//         break;
+//     };
+
+//     text += "The number is" + i;
+// };
+
+let textss;
+for (let i = 1; i < 10; i++) {
+    if (i === 3) { continue; }
+    textss += "The number is " + i
+}
+
+//console.log(textss);
+
+
+
+
+
+
+
+
+
+
