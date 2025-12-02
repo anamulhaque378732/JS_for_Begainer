a84ff40541f32507f6d2bba4b5edafdbc1ba0f10
Anamul Haque <anamulhaque4631@gmail.com>
2025-12-02 14:46:35 +0600

    String

---
+++ String/index.js
@@ -0,0 +1,25 @@
+
+const al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const single = 'anamul';
+const answer = "It's a apple";
+const answer2 = "It's called 'banana'";
+const text = `lorem`;
+const text2 = new String("Jhon"); // dont create String object
+const length = al.length;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
