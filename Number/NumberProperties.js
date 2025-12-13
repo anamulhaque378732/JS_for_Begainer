57946372f63b95316ce23d1390e40fa2fadf6752
Anamul Haque <anamulhaque4631@gmail.com>
2025-12-13 13:55:32 +0600

    js number

---
+++ Number/NumberProperties.js
@@ -0,0 +1,22 @@
+
+//console.log(Number.EPSILON);
+//console.log(Number.MAX_VALUE);
+//console.log(Number.MIN_VALUE);
+let x = 6;
+//console.log(x.Max_VALUE); //undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
