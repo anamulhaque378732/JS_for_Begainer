eb6a777d43c6e7d3a5040622074d6c486af66c5a
Anamul Haque <anamulhaque4631@gmail.com>
2025-12-22 21:58:26 +0600

    Date

---
+++ JSDate/JsDate.js
@@ -0,0 +1,69 @@
+const d = new Date();
+//console.log(d);
+const date = new Date("2022-03-25");
+console.log(date);
+/*
+/new Date()
+/new Date(date string)
+/new Date(year,month)
+/new Date(year,month,day)
+/new Date(year,month,day,hours)
+/new Date(year,month,day,hours,minutes)
+/new Date(year,month,day,hours,minutes,seconds)
+/new Date(year,month,day,hours,minutes,seconds,ms)
+/new Date(milliseconds)
+/
+/
+*/
+const ddd = new Date("October 13, 2014 11:13:00");
+console.log(ddd);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
